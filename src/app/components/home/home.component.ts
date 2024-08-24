af634996213ef476f1a4cb6149470ec9379a0b2c
Pedro Roque <proquemachado@gmail.com>
2024-08-24 13:57:43 -0300

    Alteração para mudança de layout e inclusão de novas imagens ao projeto

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { GalleryModalComponent } from '../gallery-modal/gallery-modal.component';
 import { ServicesComponent } from '../services/services.component';
 import { FooterComponent } from '../footer/footer.component';
+import { ImagePanelComponent } from '../image-panel/image-panel.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [GalleryModalComponent, ServicesComponent, FooterComponent],
+  imports: [GalleryModalComponent, ServicesComponent, FooterComponent, ImagePanelComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
